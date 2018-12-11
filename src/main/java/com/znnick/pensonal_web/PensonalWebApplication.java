80916a530425ccfe941938c23feffd4657eeff99
ZN_nick <znnznick@gmail.com>
2018-12-11 19:56:03 +0800

    0.0.1

---
+++ src/main/java/com/znnick/pensonal_web/PensonalWebApplication.java
@@ -0,0 +1,12 @@
+package com.znnick.pensonal_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensonalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensonalWebApplication.class, args);
+    }
+}
